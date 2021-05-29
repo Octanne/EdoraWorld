bf56a74070924b3964630b05dd9f49de147b7e2c
Simon Ledoux <simon@simon511000.fr>
2021-05-29 08:14:11 +0200

    remove unused function

--- src/main/java/eu/octanne/edora/server/economy/BankAccount.java
+++ src/main/java/eu/octanne/edora/server/economy/BankAccount.java
@@ -1,17 +1,11 @@
 package eu.octanne.edora.server.economy;
 
-import java.util.Map;
-
 public class BankAccount {
     
     private int oannes;
 
     public BankAccount(int oannes){
         this.oannes = oannes;
-    }
-
-    public static BankAccount fromMap(Map<String, Integer> map)  {
-        return new BankAccount(map.get("oannes"));
     }
 
 	public int getOannes() {
