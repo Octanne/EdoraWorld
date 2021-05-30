571e8e5f28466a2e487358ebc29c8df9b9c98624
Simon Ledoux <simon@simon511000.fr>
2021-05-30 10:36:54 +0200

    Ajout des groupes items par faction

--- src/main/java/eu/octanne/edora/item/EdoraItems.java
+++ src/main/java/eu/octanne/edora/item/EdoraItems.java
@@ -3,6 +3,7 @@ package eu.octanne.edora.item;
 import eu.octanne.edora.EdoraMain;
 import eu.octanne.edora.item.items.EcuItem;
 import eu.octanne.edora.item.items.MineralItem;
+import eu.octanne.edora.item.items.RingItem;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
@@ -22,13 +23,22 @@ public class EdoraItems {
     public static ItemGroup MINERAL_GROUP;
     @Environment(EnvType.CLIENT)
     public static ItemGroup ECONOMY_GROUP;
+    @Environment(EnvType.CLIENT)
+    public static ItemGroup KAWAN_GROUP;
+    @Environment(EnvType.CLIENT)
+    public static ItemGroup KALLANA_GROUP;
+    @Environment(EnvType.CLIENT)
+    public static ItemGroup OTHALA_GROUP;
 
     /**
      * Mineral Items
      */
     public static MineralItem SULFUR_CRYSTAL;
+    /**************Faction : Any**************/
     public static EcuItem NYLUS_ECU;
     public static EcuItem OANNES_ECU;
+    /**************Faction : Othala**************/
+    public static RingItem X_ILLUMINATION; // TODO: le nom n'est pas définif
 
     @Environment(EnvType.CLIENT)
     public static void registryItemGroups(){
@@ -40,6 +50,15 @@ public class EdoraItems {
             new Identifier(EdoraMain.MOD_ID, "economy"))
             .icon(() -> new ItemStack(NYLUS_ECU))
             .build();
+        KAWAN_GROUP = FabricItemGroupBuilder.create( // TODO: icon
+            new Identifier(EdoraMain.MOD_ID, "kawan"))
+            .build();
+        KALLANA_GROUP = FabricItemGroupBuilder.create( // TODO: icon
+            new Identifier(EdoraMain.MOD_ID, "kallana"))
+            .build();
+        OTHALA_GROUP = FabricItemGroupBuilder.create( // TODO: icon
+            new Identifier(EdoraMain.MOD_ID, "othala"))
+            .build();
     }
 
     public static void registryItems(){
@@ -48,9 +67,14 @@ public class EdoraItems {
          */
         SULFUR_CRYSTAL = new MineralItem(new Settings().group(MINERAL_GROUP));
         Registry.register(Registry.ITEM, new Identifier(EdoraMain.MOD_ID, "sulfur_crystal"), SULFUR_CRYSTAL);
+
+        /**************Faction : Any**************/
         NYLUS_ECU = new EcuItem(new Settings().group(ECONOMY_GROUP).rarity(Rarity.RARE));
         Registry.register(Registry.ITEM, new Identifier(EdoraMain.MOD_ID, "nylus_ecu"), NYLUS_ECU);
         OANNES_ECU = new EcuItem(new Settings().group(ECONOMY_GROUP).rarity(Rarity.UNCOMMON));
         Registry.register(Registry.ITEM, new Identifier(EdoraMain.MOD_ID, "oannes_ecu"), OANNES_ECU);
+
+        /**************Faction : Othala**************/
+
     }
 }
---
+++ src/main/java/eu/octanne/edora/item/items/RingItem.java
@@ -0,0 +1,11 @@
+package eu.octanne.edora.item.items;
+
+import net.minecraft.item.Item;
+
+public class RingItem extends Item{
+
+    public RingItem(Settings settings) {
+        super(settings);
+    }
+    
+}
