304425d61cc393343da82018f13701ea4c0084ae
Jiogo18 <30299784+Jiogo18@users.noreply.github.com>
2021-06-20 02:12:54 +0200

    Add Dark, Arid & Lantia Grass Block & Biome

--- src/main/java/eu/octanne/edora/biome/surfacebuilder/ConfiguredEdoraSurfaceBuilders.java
+++ src/main/java/eu/octanne/edora/biome/surfacebuilder/ConfiguredEdoraSurfaceBuilders.java
@@ -6,24 +6,39 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.gen.surfacebuilder.ConfiguredSurfaceBuilder;
+import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder;
 import net.minecraft.world.gen.surfacebuilder.SurfaceConfig;
 import net.minecraft.world.gen.surfacebuilder.TernarySurfaceConfig;
 
 public class ConfiguredEdoraSurfaceBuilders {
     // SurfaceBuilder defines how the surface of your biome looks.
     public static final ConfiguredSurfaceBuilder<TernarySurfaceConfig> JUNGLE;
-    
-    static private <SC extends SurfaceConfig> ConfiguredSurfaceBuilder<SC> register(Identifier id, ConfiguredSurfaceBuilder<SC> configuredSurfaceBuilder) {
+    public static final ConfiguredSurfaceBuilder<TernarySurfaceConfig> DARK;
+    public static final ConfiguredSurfaceBuilder<TernarySurfaceConfig> ARID;
+    public static final ConfiguredSurfaceBuilder<TernarySurfaceConfig> LANTIA;
+
+    static private <SC extends SurfaceConfig> ConfiguredSurfaceBuilder<SC> register(Identifier id,
+            ConfiguredSurfaceBuilder<SC> configuredSurfaceBuilder) {
         return Registry.register(BuiltinRegistries.CONFIGURED_SURFACE_BUILDER, id, configuredSurfaceBuilder);
     }
 
     static {
-        JUNGLE = register(new Identifier(EdoraMain.MOD_ID,"jungle"), EdoraSurfaceBuilder.JUNGLE
-            .withConfig(new TernarySurfaceConfig(EdoraBlocks.GRASS_BLOCK_JUNGLE.getDefaultState(),
-                                                 EdoraBlocks.DIRT_JUNGLE.getDefaultState(),
-                                                 EdoraBlocks.DIRT_JUNGLE.getDefaultState())));
+        JUNGLE = register(new Identifier(EdoraMain.MOD_ID, "jungle"),
+                EdoraSurfaceBuilder.JUNGLE
+                        .withConfig(new TernarySurfaceConfig(EdoraBlocks.GRASS_BLOCK_JUNGLE.getDefaultState(),
+                                EdoraBlocks.DIRT_JUNGLE.getDefaultState(), EdoraBlocks.DIRT_JUNGLE.getDefaultState())));
+        DARK = register(new Identifier(EdoraMain.MOD_ID, "dark"),
+                SurfaceBuilder.DEFAULT
+                        .withConfig(new TernarySurfaceConfig(EdoraBlocks.GRASS_BLOCK_DARK.getDefaultState(),
+                                EdoraBlocks.DIRT_DARK.getDefaultState(), EdoraBlocks.DIRT_DARK.getDefaultState())));
+        ARID = register(new Identifier(EdoraMain.MOD_ID, "arid"),
+                SurfaceBuilder.DEFAULT
+                        .withConfig(new TernarySurfaceConfig(EdoraBlocks.GRASS_BLOCK_ARID.getDefaultState(),
+                                EdoraBlocks.DIRT_ARID.getDefaultState(), EdoraBlocks.DIRT_ARID.getDefaultState())));
+        LANTIA = register(new Identifier(EdoraMain.MOD_ID, "lantia"),
+                SurfaceBuilder.DEFAULT
+                        .withConfig(new TernarySurfaceConfig(EdoraBlocks.GRASS_BLOCK_LANTIA.getDefaultState(),
+                                EdoraBlocks.DIRT_LANTIA.getDefaultState(), EdoraBlocks.DIRT_LANTIA.getDefaultState())));
     }
-
-
 
 }
